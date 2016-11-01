650b267ba92a33a371d6ef3091e34a7b6caabcfd
Ubuntu <vsriniv3@illinois.edu>
2016-11-01 17:41:16 -0500

    Working integration for LeM and MeM

--- src/MeM.m
+++ src/MeM.m
@@ -5,25 +5,28 @@ if count(P,pp) == 0
 end
 
 import PsiInterface.*
+    disp('Matlab running')
 
     in1 = py.PsiInterface.PsiInput('MeM_input1');
     in2 = py.PsiInterface.PsiInput('MeM_input2');
     in3 = py.PsiInterface.PsiInput('MeM_input3');  
     out = py.PsiInterface.PsiOutput('MeM_output');
-
     Input1 = in1.recv()
     Input2 = in2.recv()
+
+    while 1
+    disp('Matlab loop start')
     Input3 = in3.recv()
-    
+    disp('Matlab loop receive')   
+
     global Arate;
     global TandArateout;
-
     MeM_model(Input1, Input2,Input3);
-    
     Outdata = num2str(Arate);
     % Outdata = num2str(TandArateout);
-
     sOutput = sprintf(Outdata);
     out.send(sOutput);
+    disp('Matlab loop ends')
+    end
 
 exit();
